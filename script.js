7d4eb6e6ccd99cc9efb65ac2d8d2b957c7b0e0ba
jonathanplin <jonathan3.lin@torontomu.ca>
2024-11-06 02:01:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// Toggle Navigation Menu
-function toggleMenu() {
-    const navMenu = document.getElementById('navMenu');
-    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
-  }
-  
-  // Form Submission
-  function submitForm(event) {
-    event.preventDefault();
-    alert("Thank you! Your message has been sent.");
-    document.getElementById("contact-form").reset();
-  }
-
-  // JavaScript function to toggle the navigation menu
+// JavaScript function to toggle the navigation menu
 function toggleMenu() {
     const navMenu = document.getElementById("navMenu");
     navMenu.classList.toggle("open");
