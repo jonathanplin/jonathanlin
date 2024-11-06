53a73261afe8d1147c443c26a30cea9d403b4230
jonathanplin <jonathan3.lin@torontomu.ca>
2024-11-06 02:48:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Function to toggle the navigation menu when the hamburger icon is clicked
 function toggleMenu() {
     const navMenu = document.getElementById("navMenu");
-    navMenu.classList.toggle("open");  // Toggle the "open" class to show/hide the menu
+    const menuIcon = document.getElementById("menuIcon");
+    
+    // Toggle the "open" class to show/hide the menu
+    navMenu.classList.toggle("open");
+  
+    // Change the hamburger icon to an "X" when the menu is open
+    if (navMenu.classList.contains("open")) {
+      menuIcon.innerHTML = "&#10005;";  // "X" icon when menu is open
+    } else {
+      menuIcon.innerHTML = "&#9776;";  // Hamburger icon when menu is closed
+    }
   }
